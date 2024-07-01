424d8cf1976e2b8d7298cd7880da486b21739477
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-07-01 11:01:19 +0530

    add: firebase storage for uploading webm

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,7 +1,19 @@
 import path from "path";
 import fs from "fs";
 
+import { generateRandomFilename } from "@/app/helpers/generateFileName";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+import { firebaseConfig } from "@/app/secrets/firebase";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app, "gs://amimoji-7a88b.appspot.com");
+
 export async function POST(req: Request) {
+  const storageRef = ref(storage, `uploads/${generateRandomFilename(10)}`);
   const filePath = path.join(process.cwd(), "uploads", "video.webm");
 
   // Ensure the uploads directory exists
@@ -11,12 +23,21 @@ export async function POST(req: Request) {
 
   const files = await req.formData();
   const file = files.get("file") as Blob;
+  if (file.size < 100) {
+    return Response.json({ message: "File size too small" }, { status: 400 });
+  }
   const fileArray = file.arrayBuffer();
 
   // Write the blob (file) to the specified filePath
   try {
     fs.writeFileSync(filePath, Buffer.from(await fileArray));
-    return Response.json({ message: "File uploaded successfully" });
+    const snapshot = await uploadBytes(storageRef, file);
+    const downloadURL = await getDownloadURL(snapshot.ref);
+
+    return Response.json({
+      message: "File uploaded successfully",
+      url: downloadURL,
+    });
   } catch (err) {
     console.error("Error saving file:", err);
     return Response.json({ message: "Failed to save file" }, { status: 500 });
