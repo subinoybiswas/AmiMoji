89c864f177f589348dd94d579c25f7c630cc259a
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-07-03 23:58:11 +0530

    add: vercel analytics

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,10 +1,6 @@
-import path from "path";
-import fs from "fs";
-
 import { generateRandomFilename } from "@/app/helpers/generateFileName";
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-
 import { firebaseConfig } from "@/app/secrets/firebase";
 
 // Initialize Firebase
@@ -15,12 +11,11 @@ export async function POST(req: Request) {
   const storageRef = ref(storage, `uploads/${generateRandomFilename(10)}`);
   const files = await req.formData();
   const file = files.get("file") as Blob;
-  
+
   if (file.size < 100) {
     return Response.json({ message: "File size too small" }, { status: 400 });
   }
 
-  // Write the blob (file) to the specified filePath
   try {
     const snapshot = await uploadBytes(storageRef, file);
     const downloadURL = await getDownloadURL(snapshot.ref);
